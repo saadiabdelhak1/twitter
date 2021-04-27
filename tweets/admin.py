3ffa383555f669d1b820bf094746d8594caf4a4b
Saadi <hardyhar06@gmail.com>
2021-04-27 12:17:02 -0500

    first commit

--- tweets/admin.py
+++ tweets/admin.py
@@ -3,6 +3,6 @@
 # Register your models here.
 
 from django.contrib import admin
-# from .models import tw_tweet
+from .models import tw_tweet
 
-# admin.site.register(tw_tweet)
+admin.site.register(tw_tweet)
--- twitter/settings.py
+++ twitter/settings.py
@@ -74,14 +74,20 @@ WSGI_APPLICATION = 'twitter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'twitter_Django',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'twitter_Django',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
